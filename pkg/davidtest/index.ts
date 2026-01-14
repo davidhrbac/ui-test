cb52d85db4060413c27439063e99819ebec0795e
David Hrbáč <david@hrbac.cz>
2026-01-14 23:34:05 +0100

    davidtest: register product via DSL (fix build, generate UMD)

--- pkg/davidtest/index.ts
+++ pkg/davidtest/index.ts
@@ -1,20 +1,16 @@
-import { IPlugin } from '@shell/core/types';
 import { importTypes } from '@rancher/auto-import';
+import { IPlugin } from '@shell/core/types';
+import DavidTestPage from './pages/DavidTest.vue';
 
 export default function(plugin: IPlugin): void {
   importTypes(plugin);
-  plugin.metadata = require('./package.json');
 
+  plugin.metadata = require('./package.json');
   plugin.addProduct(require('./product'));
 
   plugin.addRoute({
-    name: 'davidtest',
-    path: '/davidtest',
-    component: () => import('./pages/DavidTest.vue'),
-    meta: {
-      product: 'davidtest',
-      cluster: false
-    }
+    name:      'davidtest',
+    path:      '/davidtest',
+    component: DavidTestPage
   });
 }
-
